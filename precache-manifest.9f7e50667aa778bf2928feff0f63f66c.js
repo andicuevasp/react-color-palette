586ba40bf868d344cfac67a432bb941bac4b8ecb
andicuevasp <andicuevasp@gmail.com>
2021-09-10 18:09:59 +0200

    Updates

---
+++ precache-manifest.9f7e50667aa778bf2928feff0f63f66c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "52c82b356e33aa90b556",
+    "url": "/react-color-palette/static/css/main.ea84ce00.chunk.css"
+  },
+  {
+    "revision": "52c82b356e33aa90b556",
+    "url": "/react-color-palette/static/js/main.5abe2220.chunk.js"
+  },
+  {
+    "revision": "4bf4f10fbfea94954f90",
+    "url": "/react-color-palette/static/js/runtime~main.27d239de.js"
+  },
+  {
+    "revision": "4d35953c7bfb730e9e48",
+    "url": "/react-color-palette/static/js/2.2fadb762.chunk.js"
+  },
+  {
+    "revision": "40d9480d501228296572a9226d6e6ea5",
+    "url": "/react-color-palette/index.html"
+  }
+];
